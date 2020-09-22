91f8e9e7008081ca73ee2e86e2c9a1306fc38772
gauravthorath <gauravjobs25@gmail.com>
2020-09-23 05:01:16 +0530

    Updates for functional Component

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Greet from "./components/greet";
 
 function App() {
   return (
     <div className="App">
-    
+    <Greet></Greet>
     </div>
   );
 }
--- hello-world/src/components/greet.js
+++ hello-world/src/components/greet.js
@@ -1,8 +1,12 @@
-import React  from 'react'; 
+import React  from 'react'
 
-function Greet ()
-{
-    return <h1> Hello Gaurav!!</h1>
-}
+//Plain functin wirtting method
+// function Greet ()
+// {
+//     return <h1> Hello Gaurav!!</h1>
+// }
+
+//ES6 arrow funtion syntax
+export const Greet = () => <h1> Hello Comp!!</h1>
 
 export default Greet;
